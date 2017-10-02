6ec3695129a2eebf1844d8e09a39ec4b49cf622c
lucaszhang2017 <lucaszhang2017@outlook.com>
2017-10-01 22:13:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.quakereport;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EarthquakeActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    private static final String SAMPLE_JSON_RESPONSE = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.earthquake_activity);
+
+        // Get the list of earthquakes from {@link QueryUtils}
+        // ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+
+        // Find a reference to the {@link ListView} in the layout
+         ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        // Create a new adapter that takes the list of earthquakes as input
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(adapter);
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected earthquake.
+        //earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                // Find the current earthquake that was clicked on
+//                Earthquake currentEarthquake = adapter.getItem(position);
+//
+//                // Convert the String URL into a URI object (to pass into the Intent constructor)
+//                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+//
+//                // Create a new intent to view the earthquake URI
+//                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+//
+//                // Send the intent to launch a new activity
+//                startActivity(websiteIntent);
+//            }
+//        });
+
+        TsunamiAsyncTask task = new TsunamiAsyncTask();
+        task.execute(SAMPLE_JSON_RESPONSE);
+    }
+
+    private class TsunamiAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+        protected List<Earthquake> doInBackground(String... urls) {
+            if(urls.length< 1 || urls[0] == null) {
+                return null;
+            }
+
+            // Perform the HTTP request for earthquake data and process the response.
+            List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
+
+            return result;
+
+        }
+
+        protected void onPostExecute(Earthquake result) {
+            if(result == null ) {
+                return;
+            }
+
+        }
+
+    }
+}
